c6e2cde6b5c4ed5337704485e80c37c3c41bb2fd
Tharisha Perera <tharisha.perera@cammsgroup.com>
2023-09-11 17:44:43 +0530

    added data table in frontend

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,21 @@
 import express, { json } from "express";
-import { PORT, MONGODB_URL } from "./config.js";
 import mongoose from "mongoose";
-import booksRoutes from "./routes/bookRoutes.js";
 import cors from "cors";
+
+import { PORT, MONGODB_URL } from "./config.js";
+import booksRoutes from "./routes/bookRoutes.js";
 
 const app = express();
 
 // Middleware for parsing request
 // Option 1: Allow all origins with default of CORS(*)
-// app.use(cors());
+app.use(cors());
 // Option 2: Allow custom origins
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-}))
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }))
 
 
 app.get("/", (req, res) => {
@@ -28,7 +29,7 @@ app.use('/books', booksRoutes);
 mongoose
   .connect(MONGODB_URL)
   .then(() => {
-    console.info("Database connection successfuull");
+    console.info("Database connection successful");
 
     app.listen(PORT, () => {
       console.log(`Backend is running at http://localhost:${PORT}`);
